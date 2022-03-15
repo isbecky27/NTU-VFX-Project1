a4ddfead583dd365e46ef58691706c22d1b627dc
isbecky27 <is.becky27@gmail.com>
2022-03-15 16:00:12 +0800

    add local operator tone mapping

--- tone_mapping.py
+++ tone_mapping.py
@@ -19,10 +19,58 @@ def global_operator(Lw, delta = 0.000001, a = 0.5, Lwhite = 200):
 
     return img
 
-def local_operator(hdr):
+def local_operator(Lw, delta = 0.000001, a = 0.5):
     '''
-    TODO
+    Ld = Lm / (1 + Lblur_smax)
     '''
+    ## calculate Lm
+    Lw_bar = np.exp(np.mean(np.log(delta + Lw)))
+    Lm = (a / Lw_bar) * Lw
+    
+    ## calculate Lblur_smax
+    Lblur_smax = np.zeros(Lw.shape)
+    for i in range(3):
+        Lblur_smax[:, :, i] = gaussian_blur(Lm[:, :, i]) 
+    
+    ## calculate Ld
+    Ld = Lm / (1 + Lblur_smax)
+
+    img = np.clip(np.array(Ld * 255), 0, 255).astype('uint8')
+
+    return img
+
+def gaussian_blur(Lm, s = 35, phi = 8, a = 0.5, e = 0.01):
+    '''
+    Lblur_s = Lm x Gs (convolution)
+    Vs = (Lblur_s - Lblur_s+1) / ((2 ** Φ) * a / (s ** 2) + Lblur_s)
+    Smax = |Vsmax| < ε
+    '''
+
+    h, w = Lm.shape
+    Lblur_smax = np.zeros((h, w))
+
+    Lblur_s_list = [cv2.GaussianBlur(Lm, (i, i), 0) for i in range(1, s+1, 2)]
+    Lblur_s_list.insert(0, Lm) # add origin Lm
+    Lblur_s_list = np.array(Lblur_s_list).transpose(1, 2, 0)
+    print(Lblur_s_list.shape)
+
+    Vs_list = np.array([np.abs(Lblur_s_list[:, :, idx] - Lblur_s_list[:, :, idx + 1]) / ((2 ** phi) * a / (ss ** 2) + Lblur_s_list[:, :, idx]) for idx, ss in enumerate(range(1, s, 2))])
+    Vs_list_max = np.argmax(Vs_list > e, axis = 0)
+    Vs_list_max -= 1 
+    Vs_list_max[Vs_list_max < 0] = 0
+    print(Vs_list_max)
+
+    i, j = np.ogrid[:h, :w]
+    Lblur_smax = Lblur_s_list[i, j, Vs_list_max]
+    
+    ## too slow
+    # for i in range(h):
+    #     for j in range(w):
+    #         Vs_list = [np.abs(Lblur_s_list[i, j, idx] - Lblur_s_list[i, j, idx + 1]) / ((2 ** phi) * a / (ss ** 2) + Lblur_s_list[i, j, idx]) for idx, ss in enumerate(range(1, s, 2))]
+    #         Vs_idx = np.argmin(Vs_list)
+    #         Lblur_smax[i, j] = Lblur_s_list[i, j, Vs_idx]
+    
+    return Lblur_smax
 
 def gamma_correction(img, r):
 	'''
@@ -95,15 +143,21 @@ if __name__ == '__main__':
     response = calibrate.process(imgs, times)
     merge_debevec = cv2.createMergeDebevec()
     hdr = merge_debevec.process(imgs, times, response)
-
+    # hdr = cv2.imread('hdr.hdr',-1)
+    # hdr = cv2.imread('./result/desk/radiance_debecvec.hdr', -1)
+    
     ## tone mapping
     # tone_mapping_using_package(hdr, 'Drago')
     # tone_mapping_using_package(hdr, 'Mantiuk')
     # tone_mapping_using_package(hdr, 'Reinhard')
     # tone_mapping_using_package(hdr)
-
     ldr = global_operator(hdr)
-    cv2.imwrite('global.png', ldr)
+    cv2.imwrite('desk_global.png', ldr)
+
+    ldr = local_operator(hdr)
+    cv2.imwrite('desk_local.png', ldr)
+    
     cv2.imshow('Tone Mapping', ldr)
     cv2.waitKey(0)
+
     
